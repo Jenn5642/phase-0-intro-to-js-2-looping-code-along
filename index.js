2d8a05ee850356fc68d970891cba19aef15c5bc9
Jenn Robbins <Jennrobbins3@gmail.com>
2022-01-25 23:24:07 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+
+let names = ["Charlie", "Samip", "Ali"];
+function writeCards(names) {
+    const messages = [] 
+for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+}
+return messages;
+}
+console.log(writeCards(names))
+
+
+function countDown() {
+    let int = 10
+while (int >= 0) {
+    console.log(int);
+    int--
+}
+
+// const name = "Jenn"
+// const job = "project manager"
+// function sayHi(name) {
+//     console.log(`hi ${name}`)
+// }
+// sayHi(name)
+// sayHi("Brian")
+}
